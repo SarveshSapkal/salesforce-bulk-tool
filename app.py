bb2411c532053a25f5da9e66a82a1fc6762dcf70
Sarvesh Sapkal <sarveshsapkal8585@gmail.com>
2026-04-25 13:24:06 +0530

    app.py

--- app.py
+++ app.py
@@ -82,6 +82,8 @@ def bulk_upload_to_salesforce(instance_url,
     return job_id
 
 def get_job_status(instance_url, access_token, job_id):
+    if "errorMessage" in s and s["errorMessage"]:
+    st.error(f"Salesforce says: {s['errorMessage']}")
     url = f"{instance_url}/services/data/v59.0/jobs/ingest/{job_id}"
     return requests.get(url, headers={"Authorization": f"Bearer {access_token}"}).json()
 
