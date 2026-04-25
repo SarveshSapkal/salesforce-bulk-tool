3bdafe4e7c4e53da263924a963e6d77ab09cdf31
Sarvesh Sapkal <sarveshsapkal8585@gmail.com>
2026-04-25 11:01:57 +0530

    microsoft_sql_db.py

--- microsoft_sql_db.py
+++ microsoft_sql_db.py
@@ -11,122 +11,23 @@ def get_connection():
     )
     return conn
 
+def save_mapping(*args,**kwargs):
+    pass
 
-# Save Mapping
-def save_mapping(object_name, mapping_dict):
-    conn = get_connection()
-    cursor = conn.cursor()
+def load_mapping(*args,**kwargs):
+    return {}
 
-    for csv_col, sf_field in mapping_dict.items():
-        cursor.execute("""
-            MERGE mappings AS target
-            USING (SELECT ? AS object_name, ? AS csv_column) AS source
-            ON target.object_name = source.object_name 
-               AND target.csv_column = source.csv_column
-            WHEN MATCHED THEN
-                UPDATE SET sf_field = ?
-            WHEN NOT MATCHED THEN
-                INSERT (object_name, csv_column, sf_field)
-                VALUES (?, ?, ?);
-        """, (object_name, csv_col, sf_field, object_name, csv_col, sf_field))
+def save_upload_history(*args,**kwargs):
+    pass
 
-    conn.commit()
-    conn.close()
+def get_upload_istory(*args,**kwargs):
+    return None
 
+def save_user(*args,**kwargs):
+    pass
 
-# Load Mapping
-def load_mapping(object_name):
-    conn = get_connection()
-    cursor = conn.cursor()
+def get_user(*args,**kwargs):
+    return None
 
-    cursor.execute(
-        "SELECT csv_column, sf_field FROM mappings WHERE object_name = ?",
-        object_name
-    )
-
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return {row[0]: row[1] for row in rows}
-
-
-# Save History
-def save_upload_history(file_name, object_name, success, failed):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute(
-        "INSERT INTO uploads (file_name, object_name, success_count, failed_count) VALUES (?, ?, ?, ?)",
-        file_name, object_name, success, failed
-    )
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# Get History
-def get_upload_history():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM uploads ORDER BY created_at DESC")
-
-    columns = [col[0] for col in cursor.description]
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return [dict(zip(columns, row)) for row in rows]
-
-# Save User
-def save_user(username, client_id, client_secret, token_url):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        INSERT INTO users (username, client_id, client_secret, token_url)
-        VALUES (?, ?, ?, ?)
-    """, (username, client_id, client_secret, token_url))
-
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-
-# Get User
-def get_user(username):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
-    row = cursor.fetchone()
-
-    if row:
-        columns = [col[0] for col in cursor.description]
-        user = dict(zip(columns, row))
-    else:
-        user = None
-
-    cursor.close()
-    conn.close()
-
-    return user
-
-# Download Data 
-def save_downloaded_data(object_name, df):
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    for _, row in df.iterrows():
-        cursor.execute(
-            "INSERT INTO downloaded_data (object_name, data) VALUES (?, ?)",
-            (object_name, str(row.to_dict()))
-        )
-
-    conn.commit()
-    cursor.close()
-    conn.close()
+def save_downloaded_data(*args,**kwargs):
+    pass
